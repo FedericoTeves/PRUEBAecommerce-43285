a4eefedb3089e67881255dde7e9673a825b17b85
Federico Teves <federicoteves11@gmail.com>
2023-06-07 11:42:30 -0300

    Subiendo los primeros archivos

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Hola Nadii HSA</h1>
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <div>
+      <h2>Este es el NavbarEste es el Navbar</h2>
+      <h3>Aca algo mas del Navbar</h3>
+    </div>
+  );
+};
+export default Navbar;
